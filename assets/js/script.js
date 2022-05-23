df0be4e00ae4f5146babfbd56c23ac64fd6eb7ae
Leticia Soares de Macedo <lelesoaresmacedo3@gmail.com>
2022-05-23 16:41:05 -0300

    Ajustes gerais do projeto

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+$(document).ready(function () {
+  var money = $('#opcoes').val();
+  var campos = $('.text-orcamento');
+  if (money === '1') {
+    $.each(campos, function(index, item){
+      var campo = $(item);
+      var valor = campo.html().replace('R$ ', '').replace('.', '').replace(',', '.');
+      var novoValor = valor * 4.81
+      console.log(novoValor.toFixed(2));
+    });
+  }
+});
